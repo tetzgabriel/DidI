e5f3d85c2f7b8a383f532c7206dfe976b27a3ade
Gabriel Tetzlaf <tetz.gabriel@gmail.com>
2021-05-17 01:01:40 -0300

    Initial Commit

---
+++ Shared/DidIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DidIApp.swift
+//  Shared
+//
+//  Created by Gabriel Tetzlaf on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct DidIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

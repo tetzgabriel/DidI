5b0efbefa962c91c81073ee470502be8cc824cc4
Gabriel Tetzlaf <tetz.gabriel@gmail.com>
2021-05-17 09:19:35 -0300

    Feature: Saving Did in UserDefaults

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var didI: Bool = UserDefaults.standard.bool(forKey: "Did")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if(didI){
+            Button(
+                action: {
+                    self.didI = false
+                    UserDefaults.standard.set(self.didI, forKey: "Did")
+                },
+                   label: {
+                Text("I Did 🥳").bold()
+            }).buttonStyle(GreenButton())
+        } else {
+            Button(
+                action: {
+                    self.didI = true
+                    UserDefaults.standard.set(self.didI, forKey: "Did")
+                },
+                   label: {
+                Text("I Didn't 😔").bold()
+            }).buttonStyle(RedButton())
+        }
+    }
+    
+}
+
+struct RedButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(100)
+            .background(Color(red: 0.8, green: 0, blue: 0))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+    }
+}
+
+struct GreenButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(100)
+            .background(Color(red: 0, green: 0.5, blue: 0))
+            .foregroundColor(.white)
+            .clipShape(Circle())
     }
 }
 
